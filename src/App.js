15d5ab813a85b3f59e94395ae154d7c1e2c27c91
sam <md346@hotmail.com>
2019-09-04 12:49:24 +0200

    Version 1.05 Error Validation, Main State in App component created.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Buscador from './Buscador';
 
 
 function App() {
+
+  const [mainData, setData] = useState('');
+
   return (
     <div className ="App container">
       <div className = "jumbotron">
         <p className ="lead text-center">Image search Engine</p>
-        <Buscador/>
+        <Buscador
+        setData = {setData}
+        />
       </div>
       
       <div className = "row justify-content-center">
--- src/Buscador.js
+++ src/Buscador.js
@@ -1,17 +1,27 @@
 import React, {useState} from 'react';
+import Error from './Error';
 
-function Buscador(){
+function Buscador({setData}){
 
     const [search, setSearch] = useState('');
+    const [error, setError] = useState(false);
 
     const searchImage = (e) =>{
         e.preventDefault();
         
         // Validar 
-
+        if (search === '') {
+            setError(true);
+            return;
+        }
+        // Cambiando a false despues de validacion
+        setError(false);
         // Enviar datos a App
-
+        setData(search);
+        
     }
+    // Logica de presentacion de error, antes del return, posteriormente llamada en la maqueta.
+    const errorComponent = (error ? <Error mensaje = "Please add a search term"/>: null);
 
     return(
         <form onSubmit = {searchImage} >
@@ -21,6 +31,7 @@ function Buscador(){
                         type="text"
                         className = "form-control form-control-lg"
                         placeholder = "Busca una imagen, ejemplo: Coches"
+                        onChange = {(e) => setSearch(e.target.value)}
                     />
                 </div>
                 <div className = "form-group col-4">
@@ -31,6 +42,7 @@ function Buscador(){
                     />
                 </div>
             </div>
+            {errorComponent}
         </form>
 
     )
