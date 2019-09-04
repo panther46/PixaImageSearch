3cd9c64edf3d5a5862ba5e39a180d766e4830408
sam <md346@hotmail.com>
2019-09-04 16:13:28 +0200

    Version 1.10 connecting to API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,26 @@ function App() {
 
   const [mainData, setData] = useState('');
 
+  useEffect (() =>{
+
+    const consultarApi = async () =>{
+      if (mainData === '') return;
+      // Parametro de API imagenes a pasar por pagina, por defecto 20.
+      const imagesPerPage = 30;
+      // Key de pixabay
+      const key = '13512871-802557b46c55675c7aace1c68';
+
+      const url = `https://pixabay.com/api/?key=${key}&q=${mainData}&per_page=${imagesPerPage}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json()
+      console.log(resultado);
+
+    }
+    consultarApi();
+
+  },[mainData]);
+
   return (
     <div className ="App container">
       <div className = "jumbotron">
