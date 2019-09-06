4a5d221f4c495a76a9e2c8b421798d008e3b3d12
sam <md346@hotmail.com>
2019-09-06 11:56:15 +0200

    Version 1.32 Setting single Image Component with data

--- src/image.js
+++ src/image.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 
 const Image = ({imagen}) =>{
-    console.log(imagen);
+
+    const {largeImageURL, likes, previewURL, tags, views} = imagen;
+    
     return(
         <div className = "col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className = "card">
+                <img src = {previewURL} alt = {tags} className = "card-img.top" />
+
+                <div className = "card-body">
+                    <p className = "card-text"> {likes} Like</p>
+                    <p className = "card-text"> {views} Views</p>
+                </div>
+
+                <div className = "card-footer">
+                    <a href = {largeImageURL} rel = "noopener noreferrer" target="_blank" className = "btn btn-primary btn-block">View image</a>
+                </div>
+
             </div>
         </div>
 
