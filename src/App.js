7f5ffccc8242ac6f9d6a62b0e7073f9c0ad2f486
sam <md346@hotmail.com>
2019-09-06 15:32:46 +0200

    Version 1.45 Calculating total pages, setting next and previous component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import List from './imageList';
 
 function App() {
 
+  // Objeto principal que viene con datos del componente BUscador
   const [mainData, setData] = useState('');
+  // Objeto preparado para la grid de imagenes
   const [images, setImages] = useState([]);
+  // Estados de compaginado
+  const [currentPage, setCurrentPage] = useState(1);
+  // Estados para calcular total de paginas en base a las imagenes que suministra la API
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect (() =>{
 
@@ -17,7 +23,7 @@ function App() {
       // Key de pixabay
       const key = '13512871-802557b46c55675c7aace1c68';
 
-      const url = `https://pixabay.com/api/?key=${key}&q=${mainData}&per_page=${imagesPerPage}`;
+      const url = `https://pixabay.com/api/?key=${key}&q=${mainData}&per_page=${imagesPerPage}&page=${currentPage}`;
 
 
       // usando fecth para hacer el request, luego extraemos la respuesta con .json
@@ -27,11 +33,32 @@ function App() {
       // Guardamos en estado las imagenes procesadas
       setImages(resultado.hits);
 
+      // Calculando el total de paginas en base a la busqueda (imagenes generadas)
+      console.log(resultado);
+      // Guardamos en el estado totalPages, el resultado del calculo redondeando hacia arriba con el metodo.ceil. 
+      // Queda guardado en el estado el numero de paginas en base a las imagenes generadas.
+      setTotalPages(Math.ceil(resultado.totalHits / imagesPerPage));
+      
+
 
     }
     consultarApi();
 
-  },[mainData]);
+  },[mainData, currentPage]);
+
+// funcion para Pagina anterior (retroceder)
+const previousPage = () =>{
+
+  let newPreviousPage = currentPage - 1;
+
+  setCurrentPage(newPreviousPage)
+}
+// funcion para Pagina siguiente (siguiente)
+const nextPage = () =>{
+  let newNextPage = currentPage + 1;
+
+  setCurrentPage(newNextPage);
+}
 
   return (
     <div className ="App container">
@@ -46,6 +73,13 @@ function App() {
         <List
         images={images}
         />
+        {(currentPage === 1) ? null:(
+         <button onClick ={previousPage} type = "button" className = "btn btn-info mr-1">&laquo; Previous</button>
+        )}
+        {(currentPage === totalPages) ? null:(
+          <button onClick = {nextPage} type = "button" className = "btn btn-info mr-1">&raquo; Next &raquo;</button>
+        )}
+        
       
       </div>
 
